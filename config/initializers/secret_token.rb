8e46323c9d33ea6132a14ae681510bde3196f210
Jen Lessor <jen_lessor@yahoo.com>
2013-12-02 14:04:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swapp::Application.config.secret_token = 'fa5d3750caf9d0b5cbad8fd48d0c7b5a5f2d393872d2fd9e94f28242bb70b428167d17c2216bc1580d288a71dec67ced58827820a7a6f65d61e5f5f722808c60'
